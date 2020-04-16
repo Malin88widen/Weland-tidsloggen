b06e2580dc915a9e4c92e47a38106dcc59a6a5b4
Malin Widén <wima18dr@student.ju.se>
2020-04-16 09:27:41 +0200

    testar

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+/*
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
@@ -15,3 +16,7 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+*/
+
+const element = <h1>Hello Worlds</h1>;
+ReactDOM.render(element, document.getElementById("root"));
