bf57f0e9680be4b271eeb8bfde8f89deb4469b32
Malin Widén <wima18dr@student.ju.se>
2020-04-30 15:29:49 +0200

    api-activity

--- TimeLogg/WelandTimeLogg/WelandTimeLogg/Controllers/ActivityInputController.cs
+++ TimeLogg/WelandTimeLogg/WelandTimeLogg/Controllers/ActivityInputController.cs
@@ -2,10 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 
 namespace WelandTimeLogg.Controllers
 {
+    [Route ("api/[Controller]")]
+    [ApiController]
     public class ActivityInputController
     {
+
+
     }
 }
