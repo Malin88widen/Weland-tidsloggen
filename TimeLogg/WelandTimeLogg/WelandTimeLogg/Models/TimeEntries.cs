0511d1a7d56d8619a3d54560d2c4b123a2a7b730
Malin Widén <wima18dr@student.ju.se>
2020-05-05 15:40:53 +0200

    TimeEntries som model inlagd

---
+++ TimeLogg/WelandTimeLogg/WelandTimeLogg/Models/TimeEntries.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WelandTimeLogg.Models
+{
+    public class TimeEntries
+    {
+        public int Id { get; set; }
+
+        public DateTime Time { get; set; }
+
+        public User User { get; set; }
+
+        public Activity Activity { get; set; }
+    }
+}
