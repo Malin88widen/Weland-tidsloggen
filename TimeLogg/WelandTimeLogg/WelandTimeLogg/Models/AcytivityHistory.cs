e16014303a9643b79e7938313e341fb15ab6f599
Malin Widén <wima18dr@student.ju.se>
2020-05-05 15:36:41 +0200

    ActivityHistory Model tillagd

---
+++ TimeLogg/WelandTimeLogg/WelandTimeLogg/Models/AcytivityHistory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WelandTimeLogg.Models
+{
+    public class AcytivityHistory
+    {
+        public int Id { get; set; }
+
+        public DateTime PublishedDate { get; set; }
+
+        public User User { get; set; }
+
+        public Activity Activity { get; set; }
+    }
+}
